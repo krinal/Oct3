490576c696303c966e903d64d8228367c3a933a8
Krinal Manakiwala <krinalmanakiwala@172-30-2-234.dynapool.nyu.edu>
2013-09-26 20:22:20 -0400

    Initial Commit

---
+++ Oct3Tests/Oct3Tests.m
@@ -0,0 +1,34 @@
+//
+//  Oct3Tests.m
+//  Oct3Tests
+//
+//  Created by Krinal Manakiwala on 9/26/13.
+//  Copyright (c) 2013 Computerlab. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Oct3Tests : XCTestCase
+
+@end
+
+@implementation Oct3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
