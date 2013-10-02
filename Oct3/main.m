9518ba590ec8c2d2b4e1c289e14c5e3340f80ee9
Krinal Manakiwala <krinalmanakiwala@gmail.com>
2013-10-01 23:33:24 -0400

    New statements before call to UIApplicationMain & iPhone window with Yellow colour

--- Oct3/main.m
+++ Oct3/main.m
@@ -21,6 +21,34 @@ int main(int argc, char * argv[])
 		NSString *version = device.systemVersion;
 		NSLog(@"The version of iOS is %@.", version);
         
+        NSLog(@"This is output produced by NSLog.");
+		int i = 10;
+		NSLog(@"The value of i is %d.", i);
+		CGFloat f = 3.14159;
+		NSLog(@"The value of f is %g.", f);
+		NSString *s = @"How are you?";
+		NSLog(@"The value of s is %@.", s);
+		CGPoint p = CGPointMake(10.0, 20.0);
+		NSLog(@"The value of p is (%g, %g).", p.x, p.y);
+		CGRect r = CGRectMake(0.0, 0.0, 360.0, 480.0);
+        
+		NSLog(@"The value of r is (%g, %g), %g by %g.",
+              r.origin. x, r.origin.y,
+              r.size.width, r.size.height
+              );
+		
+        
+		//There is only one object of class UIScreen, and it already
+		//exists.  Get the address of this object.
+		UIScreen *screen = [UIScreen mainScreen];
+        
+		CGRect bounds = [screen bounds];
+		NSLog(@"Origin is (%g, %g), dimensions are %g by %g.",
+              bounds.origin.x, bounds.origin.y,
+              bounds.size.width, bounds.size.height
+              );
+
+        
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([Oct3AppDelegate class]));
     }
 }
